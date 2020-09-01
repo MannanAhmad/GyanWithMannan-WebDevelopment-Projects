6619efa698052b805be6e6a23d7aaf3824d5f526
Sayed Mannan Ahmad <mannansarsobiz@gmail.com>
2020-09-01 14:15:25 +0530

    Chatbox using html css js and php

---
+++ ChatBox/chatBox.js
@@ -0,0 +1,33 @@
+var from = null, start = 0, url = 'http://localhost/chatbox.php';
+$(document).ready(function () {
+    from = prompt("Please enter your name")
+    load();
+
+    $('form').submit(function (e) {
+        $.post(url, {
+            message: $('#message').val(),
+            from: from
+        });
+        $('#message').val('');
+        return false;
+    });
+});
+
+function load(){
+    $.get(url + '?start=' + start, function(result){
+        if(result.$items){
+            result.$items.forEach( item => {
+                start = item.id;
+                $("#messages").append(renderMessage(item));
+            });
+            $('#messages').animate({scrollTop: $('#messages')[0].scrollHeight})
+        }
+        load()
+    })
+}
+
+function renderMessage(item){
+    let time = new Date(item.created);
+    time = `${time.getHours()}:${time.getMinutes() < 10 ? '0' : ''}${time.getMinutes()}`;
+    return `<div class="msg"><p>${item.from}</p>${item.message}<span>${time}</span></div>`
+}
